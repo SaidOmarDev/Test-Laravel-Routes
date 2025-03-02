396b71b4490112ecc49ea9a7cc6f150c156c0efb
SaidOmar <55667465+SaidOmarDev@users.noreply.github.com>
2025-03-02 11:12:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,7 @@ Route::get('user/{name}', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::get('about', function () {
-    return view('pages.about');
-})->name('about');
+Route::view('about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
@@ -71,7 +69,7 @@ Route::middleware('auth')->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
+    Route::prefix('admin')->middleware('is_admin')->group(function () {
 
         // Tasks inside that /admin group:
 
