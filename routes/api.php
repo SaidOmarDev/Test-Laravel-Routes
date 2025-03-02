4bf38d5de1171023533b259ac29fb9a134e55e08
SaidOmar <55667465+SaidOmarDev@users.noreply.github.com>
2025-03-02 11:17:45 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-
+use App\Http\Controllers\Api\V1\TaskController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
